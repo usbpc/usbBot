417a47a6cd899b1c3ef587d409a067157a03aafa
usbpc <kevin.holm@tum.de>
2017-04-26 20:04:05 +0200

    Added getroleids command to list all the roles with IDs in the discord server that the command was send

--- src/main/java/commands/TestCommands.java
+++ src/main/java/commands/TestCommands.java
@@ -7,8 +7,16 @@ public class TestCommands {
 	public void ping(IMessage msg, String...args) {
 		msg.getChannel().sendMessage("pong!");
 	}
+
 	@DiscordCommand("deleteme")
 	public void deleteme(IMessage msg, String...args) {
 		msg.delete();
+	}
+
+	@DiscordCommand("getroleids")
+	public void getroleids(IMessage msg, String...args) {
+		StringBuilder builder = new StringBuilder();
+		msg.getGuild().getRoles().forEach(role -> builder.append(role.getName()).append(": ").append(role.getLongID()).append('\n'));
+		msg.getChannel().sendMessage("There are the IDs I found: ```" + builder.toString() + "```");
 	}
 }
