28c8f4a4dc66263b85668eefdff087485e4d2617
usbpc <kevin.holm@tum.de>
2017-04-28 23:04:48 +0200

    Added comments

--- src/main/java/commands/CommandRegisterHelper.java
+++ src/main/java/commands/CommandRegisterHelper.java
@@ -22,13 +22,17 @@ public class CommandRegisterHelper {
 		Map<String, TmpCommandContainer> commandMap = new HashMap<>();
 
 		MethodHandles.Lookup lookup = MethodHandles.lookup();
-				Arrays.stream(cl.getMethods()).filter(x -> x.isAnnotationPresent(DiscordCommand.class)).forEach(x -> {
+
+		//Gets all methods from the object that have the @DiscordCommand annotation and adds them to the commandMap
+		Arrays.stream(cl.getMethods()).filter(x -> x.isAnnotationPresent(DiscordCommand.class)).forEach(x -> {
 			try {
-				commandMap.put(x.getName(), new TmpCommandContainer(x.getAnnotation(DiscordCommand.class).value(), null ,lookup.unreflect(x).bindTo(obj)));
+				commandMap.put(x.getName(), new TmpCommandContainer(x.getAnnotation(DiscordCommand.class).value(), null, lookup.unreflect(x).bindTo(obj)));
 			} catch (IllegalAccessException e) {
 				e.printStackTrace();
 			}
 		});
+
+		//Gets all methods from the object that have the @DiscordSubCommand annotation and adds them to the commandMap
 		Arrays.stream(cl.getMethods()).filter(x -> x.isAnnotationPresent(DiscordSubCommand.class)).forEach(x -> {
 			try {
 				commandMap.put(x.getName(), new TmpCommandContainer(x.getAnnotation(DiscordSubCommand.class).name(), x.getAnnotation(DiscordSubCommand.class).parent() ,lookup.unreflect(x).bindTo(obj)));
@@ -36,13 +40,21 @@ public class CommandRegisterHelper {
 				e.printStackTrace();
 			}
 		});
-		commandMap.values().stream().filter(x -> x.parentCommand != null).forEach(x -> {
+
+		//This sets the hasChildren of the Commands in the commandMap to true that have sub commands
+		commandMap.values().stream()
+				//filters all the Top Level Commands out
+				.filter(x -> x.parentCommand != null)
+				.forEach(x -> {
 			if (commandMap.containsKey(x.parentCommand)) {
 				commandMap.get(x.parentCommand).hasChildren = true;
+			} else {
+				System.out.println("Can't find parent " + x.parentCommand);
 			}
 		});
 
 		//commandMap.forEach((name, cmdContainer) -> System.out.printf("%s: children %b, parent is %s\r\n", name, cmdContainer.hasChildren, cmdContainer.parentCommand));
+
 		//This regeisters all commands that don't have sub commands and removes them from the commandMap Map
 		Set<String> noSubCommands = new HashSet<>();
 		commandMap.entrySet().stream().filter(x -> !x.getValue().hasChildren && x.getValue().parentCommand == null)
@@ -52,13 +64,19 @@ public class CommandRegisterHelper {
 				});
 		noSubCommands.forEach(commandMap::remove);
 
-		//This is the start of my code to build the sub command tree thingy
 		while (true) {
 			TmpCommandContainer currDeepest = null;
 			int currMaxDepth = 0;
-			for (TmpCommandContainer x : commandMap.values().stream().filter(x -> x.parentCommand != null && !x.hasChildren).collect(Collectors.toCollection(HashSet::new))) {
+
+			//This gets the current deepest sub command
+			for (TmpCommandContainer x : commandMap.values().stream()
+					//this filters everything but the bottom most commands out that have a parent command
+					.filter(x -> x.parentCommand != null && !x.hasChildren)
+					.collect(Collectors.toCollection(HashSet::new)))
+			{
 				TmpCommandContainer tmp = x;
 				int depthCount = 0;
+				//This count how deep the bottom most command is
 				while (tmp.parentCommand != null) {
 					tmp = commandMap.get(tmp.parentCommand);
 					depthCount++;
@@ -69,19 +87,23 @@ public class CommandRegisterHelper {
 				}
 			}
 
+			//this breaks out of the loop when no leave command ist found anymore
 			if (currDeepest == null) break;
 
+
 			String curSearchParentName = currDeepest.parentCommand;
-			TmpCommandContainer currParent = commandMap.get(curSearchParentName);
+			TmpCommandContainer currParent = commandMap.get(currDeepest.parentCommand);
 			currParent.subCommands = new HashMap<>();
 
-			System.out.printf("Currently searching for subcommand of %s \r\n", curSearchParentName);
+			//System.out.printf("Currently searching for subcommand of %s \r\n", curSearchParentName);
 
+			//This goes through every entry that is not a top level command and has the same parent command that our deepest command has
 			for (Map.Entry<String, TmpCommandContainer> entry : commandMap.entrySet().stream()
 					.filter(x -> x.getValue().parentCommand != null)
 					.filter(x -> x.getValue().parentCommand.equals(curSearchParentName))
 					.collect(Collectors.toCollection(HashSet::new)))
 			{
+				//Create a SubCommand and add it into the subCommands map of the parent
 				TmpCommandContainer sameParent = entry.getValue();
 				currParent.subCommands.put(sameParent.name, new SubCommand(sameParent.command, sameParent.subCommands));
 				commandMap.remove(entry.getKey());
@@ -90,6 +112,7 @@ public class CommandRegisterHelper {
 			currParent.hasChildren = false;
 		}
 
+		//adds all the top level command to the returned commands list
 		commandMap.values().forEach(x -> commands.add(new SubCommandParent(x.name, "", x.command, commandPermissions.getPermissionByName(x.name), x.subCommands)));
 
 
