5fdb6f78035115fab8eff13889d720d4cc47c7ec
usbpc <kevin.holm@tum.de>
2017-04-28 05:01:24 +0200

    Save work, I'm done for now

--- src/main/java/config/ConfigObject.java
+++ src/main/java/config/ConfigObject.java
@@ -11,9 +11,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigObject {
+    String boundTo = null;
     File configFile;
     FileReader reader;
     FileWriter writer;
+    JsonObject file;
     Map<String, JsonObject> boundObjects = new HashMap<>();
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
@@ -23,23 +25,24 @@ public class ConfigObject {
 
         try {
             reader = new FileReader(configFile);
-            //writer = new FileWriter(configFile);
+            //TODO this produces NullPointerException if the file to read is empty or the property dosen't exist
+            file = new Gson().fromJson(reader, JsonObject.class);
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public ConfigObject bindToProperty(String name) {
-        //TODO this produces NullPointerException if the file to read is empty or the property dosen't exist
-        JsonObject file = new Gson().fromJson(reader, JsonObject.class);
+        boundTo = name;
         JsonArray array = file.getAsJsonArray(name);
 
         array.forEach(x -> boundObjects.put(x.getAsJsonObject().get("name").getAsString(), x.getAsJsonObject()));
 
         return this;
     }
-    public void putObject(ConfigObject object) {
-
+    public void putObject(ConfigElement object) {
+        boundObjects.put(object.getUUID(), gson.fromJson(gson.toJson(object), JsonObject.class));
     }
 
     public <T extends ConfigElement> T getObjectbyName(String name, Type typeOfT) {
@@ -49,10 +52,17 @@ public class ConfigObject {
 
     public void closeConnections() {
         try {
-            reader.close();
-            //writer.close();
+            file.remove(boundTo);
+            JsonArray array = new JsonArray();
+            boundObjects.values().forEach(array::add);
+            file.add(boundTo, array);
+            writer = new FileWriter(configFile);
+            writer.write(gson.toJson(file));
+            writer.flush();
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
