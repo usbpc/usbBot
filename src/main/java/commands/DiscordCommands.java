cb1d9c37087314bf6bff38aa5d23e907b7587a54
usbpc <kevin.holm@tum.de>
2017-05-18 17:21:10 +0200

    Added some documentation

--- src/main/java/commands/DiscordCommands.java
+++ src/main/java/commands/DiscordCommands.java
@@ -3,12 +3,19 @@ package commands;
 import commands.core.Command;
 
 import java.util.Collection;
-import java.util.List;
 
 /**
- * Created by usbpc on 18.05.2017.
+ * This interface should be implemented by all Classes that intend to provide commands accessible from within discord.
+ *
+ * @author usbpc
+ * @since 2017-05-18
  */
 public interface DiscordCommands {
+
+    /**
+     *
+     * @return Collection containing all {@link commands.core.Command Commands} provided.
+     */
     public Collection<Command> getDiscordCommands();
 
 }
