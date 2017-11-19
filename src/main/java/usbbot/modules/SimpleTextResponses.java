50160abbc84387507b7f305b46041e49b6467852
kjh <spieler3001@gmail.com>
2017-11-19 11:57:20 +0100

    Added functionality to allow pictures in the custom commands!

--- src/main/java/usbbot/modules/SimpleTextResponses.java
+++ src/main/java/usbbot/modules/SimpleTextResponses.java
@@ -1,5 +1,6 @@
 package usbbot.modules;
 
+import sx.blah.discord.util.EmbedBuilder;
 import usbbot.commands.CommandHandler;
 import usbbot.commands.DiscordCommands;
 import usbbot.commands.core.Command;
@@ -14,7 +15,13 @@ import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
 import util.IMessageExtensionsKt;
 
+import java.awt.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by usbpc on 30.04.2017.
  */
@@ -35,7 +42,22 @@ public class SimpleTextResponses implements DiscordCommands {
         }
         return 0;
     }
+    private boolean isValidImage(String maybeUrl) {
+        try {
+            URI url = new URI(maybeUrl);
+            String scheme = url.getScheme();
+            if (scheme == null) return false;
+            if (!scheme.matches("https?")) return false;
+            logger.trace("Host is {}", url.getHost());
+            if (url.getHost() == null) return false;
+            logger.trace("Path is {}", url.getPath());
+            if (url.getPath().isEmpty()) return false;
+            return true;
+        } catch (URISyntaxException ex) {
+            return false;
+        }
 
+    }
     //usbbot.commands add <commandName> <Message>
     @DiscordSubCommand(parent = "commands", name = "add")
     private void commandsAdd(IMessage msg, String...args) {
@@ -50,12 +72,18 @@ public class SimpleTextResponses implements DiscordCommands {
 
 
         String message = msg.getContent().substring(msg.getContent().indexOf(args[2]) + args[2].length() + 1);
+        Matcher matcher = imagePattern.matcher(message);
+        if (matcher.find() && !isValidImage(matcher.group(1))) {
+            IMessageExtensionsKt.sendError(msg.getChannel(), "You need to specify a valid url with the use of ${image:}...");
+            return;
+        }
+
 
         DBTextCommand cmd = DBTextCommandsKt.createDBTextCommand(msg.getGuild().getLongID(),
                 args[2],
                 "whitelist",
                 "whitelist",
-                msg.getContent().substring(msg.getContent().indexOf(args[2]) + args[2].length() + 1));
+                message);
         cmd.addRoleToList(msg.getGuild().getEveryoneRole().getLongID());
         IMessageExtensionsKt.sendSuccess(msg.getChannel(), "Command `" + args[2] + "` successfully added!");
     }
@@ -83,7 +111,13 @@ public class SimpleTextResponses implements DiscordCommands {
         } else {
             DBTextCommand cmd = DBTextCommandsKt.getDBTextCommand(msg.getGuild().getLongID(), args[2]);
             if (cmd != null) {
-                cmd.editText(msg.getContent().substring(msg.getContent().indexOf(args[2]) + args[2].length() + 1));
+                String message = msg.getContent().substring(msg.getContent().indexOf(args[2]) + args[2].length() + 1);
+                Matcher matcher = imagePattern.matcher(message);
+                if (matcher.find() && !isValidImage(matcher.group(1))) {
+                    IMessageExtensionsKt.sendError(msg.getChannel(), "You need to specify a valid url with the use of ${image:}...");
+                    return;
+                }
+                cmd.editText(message);
                 IMessageExtensionsKt.sendSuccess(msg.getChannel(), "Changed `" + args[2] + "`!");
             } else {
                 IMessageExtensionsKt.sendError(msg.getChannel(), "`" + args[2] + "` is not a command");
@@ -108,17 +142,26 @@ public class SimpleTextResponses implements DiscordCommands {
         answer(msg, cmd);
     }
 
+    private static Pattern imagePattern = Pattern.compile("\\$\\{image:(.+?)\\}");
+
     public static void answer(IMessage msg, DBTextCommand cmd) {
         String content = cmd.getText();
         //TODO placeholder for args to be replaced by the arguments given when called
-            /*
-            * possible placeholders: author, args
-            *
-            *
-            *
-            * */
-        content = content.replaceAll("§§AUTHOR§§", msg.getAuthor().mention(true));
-        IMessageExtensionsKt.sendSuccess(msg.getChannel(), content);
+        /*
+        * possible placeholders: author, args
+        *
+        *
+        *
+        * */
+        EmbedBuilder embed = new EmbedBuilder().withColor(Color.GREEN);
+        content = content.replaceAll("\\$\\{author\\}", msg.getAuthor().mention(true));
+        Matcher matcher = imagePattern.matcher(content);
+        if (matcher.find()) {
+            embed.withImage(matcher.group(1));
+            content = matcher.replaceAll("");
+        }
+        embed.withDescription(content);
+        IMessageExtensionsKt.bufferedSend(msg.getChannel(), embed.build());
     }
 
 
